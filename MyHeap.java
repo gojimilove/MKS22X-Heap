817762395076147c3def8e7e1e36c25ba5d54823
Jessica Kim <jkim00@stuy.edu>
2019-04-29 11:44:18 -0400

    empty methods

---
+++ MyHeap.java
@@ -0,0 +1,12 @@
+public class MyHeap {
+  private static void pushDown(int[]data,int size,int index) {
+    //preconditions: index is between 0 and size-1 (inclusive), size is between 0 and data.length-1 (inclusive)
+  }
+
+  private static void pushUp(int[]data,int index) {}
+
+  public static void heapify(int[]) {}
+
+  public static void heapsort(int[]) {}
+
+}
